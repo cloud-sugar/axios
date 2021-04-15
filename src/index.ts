96a4a0f9515a714264c5e206d6a4c038ae0c50ed
Craig Younker <cryo2010@gmail.com>
2021-04-15 01:05:35 -0700

    feat(index): enabled self-signed certs in development

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,17 @@ import axios, {AxiosInstance} from 'axios';
 import axiosRetry from 'axios-retry';
 import { Agent } from 'https';
 
-axios.defaults.timeout = 15 * 1000;
+const instance = axios.create({
+  timeout: 15 * 1000, 
+  httpsAgent: new Agent({
+    rejectUnauthorized: process.env.NODE_ENV !== 'development'
+  })
+});
 
-axiosRetry(axios, {
+axiosRetry(instance, {
   retries: 4, 
   retryDelay: axiosRetry.exponentialDelay
 });
 
-/**
- * Enables the use of self-signed SSL certificates for development
- */
-export function allowUnauthorized(axios: AxiosInstance): AxiosInstance {
-  const httpsAgent = new Agent({
-    rejectUnauthorized: false,
-  })
-  axios.defaults.httpsAgent = httpsAgent;
-  return axios;
-}
-
-export default axios;
+export default instance;
 
